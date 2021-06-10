b16a91f5026a18aa55a30c70d2a7910cf8d615b6
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-06-10 14:53:13 -0300

    Giro 2

--- src/TranslationsCountries.js
+++ src/TranslationsCountries.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import FetchCountries from './services/countries';
+import fetchCountries from './services/countries';
 
 class TranslationsCountries extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      countries: [],
+    }
+    this.getCountries = this.getCountries.bind(this)
+  }
+
+  componentDidMount(){
+    this.getCountries()
+  }
+
+  async getCountries(){
+    const countries = await fetchCountries()
+    this.setState({
+      countries: countries.name
+    })
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div>
--- src/services/countries.js
+++ src/services/countries.js
@@ -1,2 +1,4 @@
-export const fetchCountries = () =>
+const fetchCountries = () =>
   fetch('https://restcountries.eu/rest/v2/all').then(res => res.json());
+
+export default fetchCountries;
