b1a253146e519f22df6f1328d2e5c5d1f78d6899
Marco Galindo <marco1408@gmail.com>
2021-06-10 14:43:38 -0300

    countries recebido via requisicao api

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { fetchCountries } from './services/countries';
 
 class Countries extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: [],
+    }
+  }
+
+  componentDidMount() {
+    this.fetchAllCountries();
+  }
+
+  async fetchAllCountries() {
+    const countriesList = await fetchCountries();
+    this.setState({
+      countries: countriesList,
+    })
+  }
+
   render() {
     return (
       <h1>Countries</h1>
