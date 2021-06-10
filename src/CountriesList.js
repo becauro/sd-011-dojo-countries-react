8bac9576955badef4e4df003ddd4cc68d43ce4fa
Rodolfo <rodolforrcont@gmail.com>
2021-06-10 14:40:51 -0300

    Estrutura Class

--- src/CountriesList.js
+++ src/CountriesList.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import CountriesList from './services/countries';
 
 class CountriesList extends React.Component {
-  
+  constructor(){
+    super()
+    this.state({
+
+    })
+  }
+
+  componentDidMount(){
+
+  }
+
+  async createCountriesList(){
+    const countries = await CountriesList()
+
+  }
+
+  render(){
+    return (
+      
+    )
+
+  }
 }
+
+export default CountriesList;
