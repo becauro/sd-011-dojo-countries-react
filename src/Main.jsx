c796163449e6b16708a6b4490dcd1388ee4880e5
murilodospacos <murilopacos@gmail.com>
2021-06-10 14:50:36 -0300

    trocando piloto

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,9 +2,25 @@ import { Component } from 'react';
 import { fetchCountries } from './services/countries'
 
 export default class Main extends Component {
+  constructor() {
+    super()
 
-  async componentDidMount () {
-   console.log(await fetchCountries());
+    this.state = {
+      paises: [],
+    }
+
+  }
+
+   async componentDidMount () {
+    await this.getNameCountry();
+   console.log(this.state.paises)
+  }
+
+  async getNameCountry() {
+    const paises = await fetchCountries();
+    this.setState({
+      paises
+    })
   }
 
   render () {
