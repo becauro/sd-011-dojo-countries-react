8aeb76dbe04d6bd4bcb6e2f892cd04fba2939eac
Tulio Sploradori <tuliospl@gmail.com>
2021-06-10 14:40:40 -0300

    Inicio

---
+++ src/CountriesList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Countries from './services/countries';
+
+
+
+const CountriesList = () => {
+  console.log();
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+
+export default CountriesList;
