4befade7bdb9a755d61d8ea9096fb5d63edc8e8a
Thiago Gomes <thiagog.macedom@gmail.com>
2021-06-10 14:40:23 -0300

    troca de piloto

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import './App.css';
 import apiCountries from './services/countries';
 
 class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      countryList: [],
+    };
+
+    this.getCountry = this.getCountry.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCountry();
+  }
+
   async getCountry() {
     const country = await apiCountries.fetchCountries();
+    this.setState({
+      countryList: country,
+    });
   }
+
   render() {
+    const { countryList } = this.state;
+    console.log(countryList);
     return (
       <main>
         <h1>Lista de países</h1>
