776861d36c2ce875cd3fba1c9493f5ebe0111998
Alberto Candido <albertocandido98@gmail.com>
2021-06-10 15:57:19 -0300

    Finaliza requisito 3

--- src/CountriesList.jsx
+++ src/CountriesList.jsx
@@ -6,16 +6,28 @@ export default class CountriesList extends Component {
   constructor() {
     super()
     this.state = {
-      loading: true,  
+      loading: true,
     }
+    this.filterCountrie = this.filterCountrie.bind(this);
   }
 
   async fetchC() {
     const list = await fetchCountries()
-    console.log(list[0]);
+    // console.log(list[0]);
     this.setState({
       countries: list,
+      backup: list,
       loading:false,
+    })
+  }
+
+
+  filterCountrie({target : {value}}) {
+    console.log(value);
+    const { backup } = this.state;
+    const filteredList = backup.filter(({ translations }) => translations.pt.toUpperCase().includes(value.toUpperCase()));
+    this.setState({
+      countries: filteredList,
     })
   }
 
@@ -27,6 +39,7 @@ export default class CountriesList extends Component {
     const { loading, countries } = this.state;
     return loading ? <Loading/> : (
       <div>
+        Filtrar países <input type="text" onChange={this.filterCountrie}></input>
         {countries.map(({name, translations, flag})=> {
           // console.log(currencies);
         return <p key={ name }>{ translations.pt } <img className="img-flag" src={flag} alt={name} /></p>
