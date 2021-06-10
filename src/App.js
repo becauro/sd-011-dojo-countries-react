f8e86e4a14ddda4b5633e07e3d90869b9f0e84fc
Jessica Arruda <sicarruda@gmail.com>
2021-06-10 14:52:34 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import './App.css';
-import * as countries from './services/countries';
+import React from "react";
+import "./App.css";
+import * as countries from "./services/countries";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+    };
+    this.allCountries = this.allCountries.bind(this);
+  }
+  componentDidMount() {
+    this.allCountries();
+  }
+
   async allCountries() {
     const apiCountries = await countries.fetchCountries();
+    this.setState({ countries: apiCountries });
+
     console.log(apiCountries);
   }
 
   render() {
+    const { countries } = this.state;
     return (
       <main>
-        <h1>Lista de países</h1>
+        
+       {countries.map((countrie)=> <h1>{countrie.translations.br} </h1>)}
 
       </main>
-    )
+    );
   }
 }
 
