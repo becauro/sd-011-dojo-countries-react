9f0190b0d89eb6b6e94e266cf4525ae1e4ce9e8a
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-06-10 14:41:40 -0300

    Cria CountryList

---
+++ src/components/CountryList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CountryList extends React.Component {
+
+  render() {
+    return (
+      <ul>
+        
+      </ul>
+    )
+  }
+}
+
+export default CountryList;
