96c8abd11c1307fd077334013cfc4c6eb2d15b0c
Diogo Augusto <diogotrescastro@gmail.com>
2021-06-10 14:50:53 -0300

    piloto 3

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -2,6 +2,21 @@ import React, { Component } from 'react'
 import  { fetchCountries } from '../services/countries'
 
 export default class CountriesList extends Component {
+
+    constructor() {
+      super();
+      this.state = {
+        countries: [],
+      };
+
+    }
+  componentDidMount {
+    this.setState({
+      countries: 
+    })
+    fetchCountries()
+  }
+
   render() {
     return (
       <div>
