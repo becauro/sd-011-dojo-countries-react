6d7c73817578894d8d58d915b5ceeba78b0d9a55
marlon307 <marlon-ramosb@hotmail.com>
2021-06-10 14:40:29 -0300

    Marlon

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <main>
         <h1>Lista de países</h1>
+        <ul>
+
+        </ul>
       </main>
     )
   }
