b3ec6ab5d85f39ed8bc918d96774953713e23a5c
fsrocha-dev <contato.frankrocha@gmail.com>
2021-06-10 12:42:54 -0300

    Init React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Lista de países</h1>
+      </main>
+    )
+  }
+}
+
+export default App;
