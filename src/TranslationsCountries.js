b622979e6c0d80e53301b9a54a16d78022b2d4aa
Laura Gusmao <laurargus@gmail.com>
2021-06-10 14:36:36 -0300

    Adiciona componete

---
+++ src/TranslationsCountries.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FetchCountries from './services/countries';
+
+class TranslationsCountries extends React.Component {
+  render() {
+    return (
+      <div>
+        Translations
+      </div>
+    )
+  }
+}
+
+export default TranslationsCountries
