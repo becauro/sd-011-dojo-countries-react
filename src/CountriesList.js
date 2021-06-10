9804c181804e25a1214503f652d5bc89ae61ff55
Wilquer82 <wilquer.fig@gmail.com>
2021-06-10 14:33:14 -0300

    Troca de guarda

---
+++ src/CountriesList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Countries from './services/countries';
+
+class Countries extends React.Component {
+    componentDidMount() {
+        const { fetchCountries } = Countries;
+    }
+    
+    render() {
+        return 
+
+        
+    }
+}
