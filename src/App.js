9450aa45adbfa259c6f5bc6cc087e659b4c50edf
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-10 17:33:43 -0300

    Tentativa de requisito 3

--- src/App.js
+++ src/App.js
@@ -5,38 +5,62 @@ import { fetchCountries } from './services/countries';
 class App extends React.Component {
   constructor() {
     super();
-
     this.state = {
+      input: '',
       countries: [],
+      filter: [],
     };
 
+    this.filterInput = this.filterInput.bind(this); 
   }
 
-async componentDidMount() {
-  const list = await fetchCountries();
-  this.setState({
-    countries: list,
-  })
-  console.log(list[0].translations.br)
-}
+  async componentDidMount() {
+    const list = await fetchCountries();
+    this.setState({
+      countries: list,
+      // filter: list,
+    });
+  }
+
+  filterInput({ target }) {
+    this.setState({
+      input: target.value,
+    });
+
+    const { input, countries } = this.state;
+
+    let filteredCountries = countries;
+
+    if (input) {
+      filteredCountries = countries.filter(({ translations: { br } }) => br.includes(input));
+       this.setState({
+         filter: filteredCountries,
+       });
+    }
+
+    if (!input) {
+      this.setState({
+        filter: countries,
+      });
+    }
+  }
 
   render() {
-
-const { countries } = this.state;
-
+    const { filter } = this.state;
+    
     return (
       <main>
         <label htmlFor="filterCountry">
-          <input type='text' name="filterCountry"></input>
+          Filtre o país
+          <input type='text' name="filterCountry" onChange={ this.filterInput }/>
         </label>
-        {countries.map((country) => {
-        return (
-          <div>
-          <h1> {country.translations.br} </h1>
-          <img src={country.flag} />
-          </div> 
+        {filter.map((country) => {
+          return (
+            <div>
+              <h1> {country.translations.br} </h1>
+              <img src={country.flag} width="35" alt={`Bandeira de ${country.translations.br}`}/>
+            </div>
           )
-           
         })}
       </main>
     )
