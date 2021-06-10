8232b28b5a71fc3be947e6e2bb16aaf8432c9196
guinhoanjos <iago.anjos@hotmail.com>
2021-06-10 14:53:00 -0300

    troca de piloto

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import * as fetchCountries from './services/countries'
 
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: [],
+    }
+  }
+
+  async getCountry() {
+    const fetch = await fetchCountries.fetchCountries()
+    const allCountries
+  }
   render() {
-    console.log(fetchCountries);
+    fetchCountries.fetchCountries().then((variavel)=> console.log(variavel))
     return (
       <main>
         <h1>Lista de países</h1>
+        <ol>
+          <li></li>
+        </ol>
       </main>
     )
   }
