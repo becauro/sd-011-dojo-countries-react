6bdb19218fc3d8775bc2f827af8bed51c8e1b1d7
Alberto Candido <albertocandido98@gmail.com>
2021-06-10 15:45:49 -0300

    Finaliza requisito 2

--- src/CountriesList.jsx
+++ src/CountriesList.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import Countries from './services/countries';
+import React, { Component } from 'react'
+import fetchCountries from './services/countries'
+import Loading from './Loading';
 
-
-
-const CountriesList = () => {
-
-  async function fetchCountries() {
-    const promise = await Countries()
-    console.log(promise);
+export default class CountriesList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loading: true,  
+    }
   }
-  fetchCountries()
-  return (
-  <div>
-    oi
-  </div>
-  );
-};
 
-export default CountriesList;
+  async fetchC() {
+    const list = await fetchCountries()
+    console.log(list[0]);
+    this.setState({
+      countries: list,
+      loading:false,
+    })
+  }
+
+  componentDidMount() {
+    this.fetchC();
+  }
+
+  render() {
+    const { loading, countries } = this.state;
+    return loading ? <Loading/> : (
+      <div>
+        {countries.map(({name, translations, flag})=> {
+          // console.log(currencies);
+        return <p key={ name }>{ translations.pt } <img className="img-flag" src={flag} alt={name} /></p>
+        })}
+      </div>
+    )
+  }
+}
