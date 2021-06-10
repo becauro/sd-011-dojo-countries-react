57cda779b05c1a904c7ae5b488ef6ddf85726f05
heyset <c.inaciomatheus@gmail.com>
2021-06-10 14:50:29 -0300

    Cria render de ul. Troca piloto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { fetchCountries } from './services/countries';
+import CountryList from './components/CountryList';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      countries: [],
+    }
+  }
+
+  async fetchCountries()
+  
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <main>
         <h1>Lista de países</h1>
+        <CountryList countries />
       </main>
     )
   }
--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -3,9 +3,11 @@ import React from 'react';
 class CountryList extends React.Component {
 
   render() {
+    const { countries } = this.props;
+
     return (
       <ul>
-        
+        { countries.map((country) => <li>country.translations.br</li>) }
       </ul>
     )
   }
