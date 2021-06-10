e44811dabf70d0534bb0e5c22791d29025a48a86
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-10 14:39:25 -0300

    Modificaçao 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import './App.css';
+import { fetchCountries } from './services/countries';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      countries: [],
+    };
+
+    
+  }
+
+componentDidMount() {
+  const list = fetchCountries().then();
+  this.setState({
+    countries: fetchCountries(),
+  })
+  // console.log();
+}
+
+
+
+
   render() {
+    
     return (
       <main>
         <h1>Lista de países</h1>
@@ -10,5 +33,5 @@ class App extends React.Component {
     )
   }
 }
-// 
+//
 export default App;
