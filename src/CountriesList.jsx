02bf3d78f93560a385046f754baa21ce7ce75646
PH Assis <pieit@live.com>
2021-06-10 14:50:34 -0300

    Passando para Alberto

--- src/CountriesList.jsx
+++ src/CountriesList.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import Countries from './services/countries';
 
-
-
 const CountriesList = () => {
-  console.log();
-  return (
-    <div>
-      
-    </div>
-  );
-}
-
+  // const list = await Countries.fetchCountries();
+  console.log(Countries);
+  return <div>oi</div>;
+};
 
 export default CountriesList;
