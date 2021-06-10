bee5416f776d9f5da79b7bc7a8644fd219420d21
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-10 15:37:17 -0300

    requisito 3 feito

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ class App extends React.Component {
     this.state = {
       api: {},
       loading: true,
+      busca: '',
     };
     this.fetchCountries = this.fetchCountries.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -21,6 +23,33 @@ class App extends React.Component {
     this.setState({ api: response, loading: false });
   }
 
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  filterCountries() {
+    const { api, busca } = this.state;
+
+    let countries = api.map(({ translations, flag }) => (
+      <li key={translations.br}>
+        <img width="30px" src={flag} alt={translations.br} />
+        {translations.br}
+      </li>
+    ));
+
+    if (busca) {
+      countries = countries.filter(({ props }) =>
+        props.children[1].toLowerCase().includes(busca.toLowerCase())
+      );
+      return countries;
+    }
+
+    return countries;
+  }
+
   render() {
     const { api, loading } = this.state;
     if (loading) {
@@ -30,14 +59,11 @@ class App extends React.Component {
     return (
       <main>
         <h1>Lista de países</h1>
-        <ul>
-          {api.map(({ translations, flag }) => (
-            <li key={translations.br}>
-              <img width="30px" src={flag} alt={translations.br} />
-              {translations.br}
-            </li>
-          ))}
-        </ul>
+        <label>
+          Busca
+          <input type="text" name="busca" onChange={this.handleChange}></input>
+        </label>
+        <ul>{this.filterCountries()}</ul>
       </main>
     );
   }
