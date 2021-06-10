b9d2b0e51f4c2c49046bd1b4ff385f66d0f79a35
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-10 17:45:42 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -11,54 +11,56 @@ class App extends React.Component {
       filter: [],
     };
 
-    this.filterInput = this.filterInput.bind(this); 
+    this.filterInput = this.filterInput.bind(this);
   }
 
   async componentDidMount() {
     const list = await fetchCountries();
     this.setState({
       countries: list,
-      // filter: list,
+      filter: list,
     });
   }
 
   filterInput({ target }) {
     this.setState({
       input: target.value,
+    }, () => {
+      const { input, countries } = this.state;
+
+      let filteredCountries = countries;
+
+      if (input) {
+        filteredCountries = countries.filter(({ translations: { br } }) => br.includes(input));
+        this.setState({
+          filter: filteredCountries,
+        });
+      }
+
+      if (!input) {
+        this.setState({
+          filter: countries,
+        });
+      }
     });
 
-    const { input, countries } = this.state;
 
-    let filteredCountries = countries;
-
-    if (input) {
-      filteredCountries = countries.filter(({ translations: { br } }) => br.includes(input));
-       this.setState({
-         filter: filteredCountries,
-       });
-    }
-
-    if (!input) {
-      this.setState({
-        filter: countries,
-      });
-    }
   }
 
   render() {
     const { filter } = this.state;
-    
+
     return (
       <main>
         <label htmlFor="filterCountry">
           Filtre o país
-          <input type='text' name="filterCountry" onChange={ this.filterInput }/>
+          <input type='text' name="filterCountry" onChange={this.filterInput} />
         </label>
         {filter.map((country) => {
           return (
             <div>
               <h1> {country.translations.br} </h1>
-              <img src={country.flag} width="35" alt={`Bandeira de ${country.translations.br}`}/>
+              <img src={country.flag} width="35" alt={`Bandeira de ${country.translations.br}`} />
             </div>
           )
         })}
