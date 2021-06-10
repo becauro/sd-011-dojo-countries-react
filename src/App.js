3846cb22ece9164aec4ca1c4d5de1809ddb93471
benlopes <rubenssouss@hotmail.com>
2021-06-10 14:50:26 -0300

    projeto incompleto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
-import fetchCountries from './services/countries'
+import * as countries from './services/countries';
  
 class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      list: [],
+    }
+    this.getList = this.getList.bind(this);
+  }
+
+  componentDidMount(){
+    this.getList();  
+  }
+
+  async getList(){
+    const countriesList = await countries.fetchCountries();
+    this.setState({
+      list: countriesList,
+    })
+  }
+
   render() {
+    const { list } = this.state;
     return (
       <main>
         <h1>Lista de países</h1>
         <ol>
-          { fetchCountries() }
+          { list }
         </ol>
       </main>
     )
