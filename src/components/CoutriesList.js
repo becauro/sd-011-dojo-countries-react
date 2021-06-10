5585d88dcfa350c7385acd4b4bf5a46d69895456
OldRoge <rogeriomrjr@icloud.com>
2021-06-10 14:40:56 -0300

    Initial commit

---
+++ src/components/CoutriesList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Countries from './countries';
+
+class CoutriesList extends React.Component {
+
+  render() {
+    return ()
+  }
+}
+
+export default CoutriesList;
