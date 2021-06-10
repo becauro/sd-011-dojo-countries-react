6104e57c12356928e3943ae8e11e55c26655f8d6
Hamander <1hamander@gmail.com>
2021-06-10 02:33:12 -0300

    Incomplete code, switching pilots

---
+++ src/components/Countries.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import countries from '../services/countries';
+
+export default class Countries extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
