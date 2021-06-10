020e9cf53ced943b47fa5b49f9fe9ce69618ed0c
Alan Gonçalves <vekio.soft@gmail.com>
2021-06-10 15:21:03 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import './App.css';
 import Countries from './Countries';
+import { fetchCountries } from './services/countries';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: [],
+    }
+  }
+
+  componentDidMount() {
+    this.fetchAllCountries();
+  }
+
+  async fetchAllCountries() {
+    const countriesList = await fetchCountries();
+    this.setState({
+      countries: countriesList,
+    })
+  }
+
   render() {
+    const { countries } = this.state;
+
     return (
       <main>
         <h1>Lista de países</h1>
-        <Countries />
+        <label htmlFor="search" >
+          Pesquise os países
+          <input type="text" name="search" id="search"/>
+        </label>
+        <Countries countries={ countries }/>
       </main>
     )
   }
--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import { fetchCountries } from './services/countries';
 import './Countries.css';
 
 class Countries extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      countries: [],
-    }
-  }
-
-  componentDidMount() {
-    this.fetchAllCountries();
-  }
-
-  async fetchAllCountries() {
-    const countriesList = await fetchCountries();
-    this.setState({
-      countries: countriesList,
-    })
-  }
 
   render() {
-    const { countries } = this.state;
+    const { countries } = this.props;
 
     return (
       <div>
