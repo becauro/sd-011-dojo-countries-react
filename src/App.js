8b12fad3f69945a0275f37e33453fa0819ce81c0
Pedro Castro <pa.coelho.castro@gmail.com>
2021-06-10 14:31:23 -0300

    Incomplete code, switching pilots

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import * as countries from './services/countries';
 
 class App extends React.Component {
+  async allCountries() {
+    const apiCountries = await countries.fetchCountries();
+    console.log(apiCountries);
+  }
+
   render() {
     return (
       <main>
         <h1>Lista de países</h1>
+
       </main>
     )
   }
