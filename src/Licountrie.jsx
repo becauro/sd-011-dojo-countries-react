db0fce7b1ea0aaf233169c5625fefafad2fa3c33
Nikolas <nikolas.silva1990@gmail.com>
2021-06-10 15:03:08 -0300

    Quarto piloto

---
+++ src/Licountrie.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Licountrie extends React.Component {
+render(){
+  return (
+  <ul>
+    <img src={ countrie.flag } />
+    <li> {countrie.name} </li>
+
+
+  </ul>
+  )
+    }
+}
+
+export default Licountrie;
