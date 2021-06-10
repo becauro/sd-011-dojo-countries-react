8d8301ada0775692eaec2664490108fa6a2ef326
Laura Gusmao <laurargus@gmail.com>
2021-06-10 14:31:20 -0300

    Iniciando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import TranslationsCountries from './TranslationsCountries';
 
 class App extends React.Component {
   render() {
     return (
       <main>
         <h1>Lista de países</h1>
+        <TranslationsCountries />
       </main>
     )
   }
