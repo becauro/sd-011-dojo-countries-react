89de1e3af16a88a9d18db5361e330ae9bba96e13
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-06-10 13:41:14 -0400

    Trocando de piloto Gustavo.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import './App.css';
 import Countries from './services/countries';
 
+
 class App extends React.Component {
   constructor() {
+    super()
 
+    this.state{
+      countries: [],
+    }
   }
+
+  componentDidMount(){
+    this.fetchCountries()
+    console.log(fetchCountries())
+  }
+
   render() {
     return (
       <main>
