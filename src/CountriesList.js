034ddfc8c2b16ceb272e143effd89c6e6ecf66d8
fernando lasmar <f3rnandolasmar@gmail.com>
2021-06-10 15:20:59 -0300

    Nao deu tempo

--- src/CountriesList.js
+++ src/CountriesList.js
@@ -2,18 +2,26 @@ import React from 'react';
 import Countries from './services/countries';
 
 class CountriesList extends React.Component {
-
+    constructor() {
+        super() 
+        this.state = {
+            countriesObj: {},
+        }
+    }
 
     async componentDidMount() {
         
         const newCountries = await Countries();
         console.log(newCountries)
+        this.setState({
+            countriesObj: newCountries,
+        })
     }
     
     render() {
         return(
             <div>
-              {/* { countriesList}   */}
+              { this.state.countriesObj.map((listaPaises) => listaPaises).map((pais) => console.log(pais)) }  
             </div>            
         )        
     }
